dc3e0065d9cdb15d0ed6ef1cc1717516f4e0942e
depakin <yisangwu@163.com>
2018-09-25 14:56:55 +0800

    del some nosue code

--- swoole_client.php
+++ swoole_client.php
@@ -1,20 +1,15 @@
 <?php
 
    //daemonize 设置为守护进程后，相对路径可能会出错，请使用绝对路径写文件。
-/*    
-wget http://pecl.php.net/get/swoole-1.9.22.tgz
-tar zxf swoole-1.9.22.tgz
-cd swoole-1.9.22
-phpize
-./configure --with-php-config=/usr/local/php/bin/php-config
-make && make install */
-
-	//创建Server对象，监听 127.0.0.1:9502端口，类型为SWOOLE_SOCK_UDP
+	/*    
+	wget http://pecl.php.net/get/swoole-1.9.22.tgz
+	tar zxf swoole-1.9.22.tgz
+	cd swoole-1.9.22
+	phpize
+	./configure --with-php-config=/usr/local/php/bin/php-config
+	make && make install */
 
 	//client
-
-	//$str = json_encode( array('time'=>time(),'0'=>'127.0.0.1:9502', 'client'=>'WOOLE_SOCK_UDP') );
-	
 	$str  = json_encode( array('logname'=>'xxxx','logstring'=>12345678) );
 	
 	while( true ){
